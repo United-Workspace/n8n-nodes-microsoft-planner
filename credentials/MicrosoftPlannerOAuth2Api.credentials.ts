37e270614406f073881a25bbfd829c9c992ddda6
Sebastian Fuss <sebastian.fuss@united-workspace.de>
2026-01-25 22:00:59 +0100

    config(credentials): expose auth and access token URLs

--- credentials/MicrosoftPlannerOAuth2Api.credentials.ts
+++ credentials/MicrosoftPlannerOAuth2Api.credentials.ts
@@ -21,14 +21,14 @@ export class MicrosoftPlannerOAuth2Api implements ICredentialType {
 		{
 			displayName: 'Authorization URL',
 			name: 'authUrl',
-			type: 'hidden',
+			type: 'string',
 			default: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
 			required: true,
 		},
 		{
 			displayName: 'Access Token URL',
 			name: 'accessTokenUrl',
-			type: 'hidden',
+			type: 'string',
 			default: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
 			required: true,
 		},
